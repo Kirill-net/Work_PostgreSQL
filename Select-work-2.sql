8fea24793b687f3c971c9eb34730d6208b60343d
Kirill Shcherbakov <scherbakovkirill@yandex.ru>
2024-02-09 18:06:36 +0300

    add_correct_select_files

--- Select-work-2.sql
+++ Select-work-2.sql
@@ -1,12 +1,11 @@
-SELECT tr_name, tr_time FROM track
-ORDER BY tr_time DESC 
-LIMIT 1
+SELECT tr_name, tr_time FROM track 
+WHERE tr_time = (SELECT MAX(tr_time) FROM track)
 
 SELECT tr_name FROM track
 WHERE tr_time >= 210
 
 SELECT col_name FROM collection 
-WHERE col_year >=2018 AND col_year <=2020 
+WHERE col_year BETWEEN 2018 AND 2020 
 
 SELECT name FROM perfomen 
 WHERE name NOT LIKE '% %'
--- Select-work-3.sql
+++ Select-work-3.sql
@@ -4,16 +4,18 @@ ORDER BY musicstile_id
 
 SELECT COUNT(tr_name) FROM track t
 JOIN album a ON t.album_id = a.id 
-WHERE al_year = 2019 OR al_year = 2020
+WHERE al_year BETWEEN 2019 AND 2020
 
 SELECT AVG(tr_time) FROM track
 GROUP BY album_id
 
-SELECT name FROM perfomen p
+SELECT DISTINCT name FROM perfomen p
+WHERE name NOT IN (
+SELECT name FROM perfomen p 
 JOIN perfomen_album pa ON p.id = pa.perfomen_id
 JOIN album a ON pa.album_id = a.id 
-WHERE al_year != 2020
-GROUP BY name
+WHERE al_year = 2020
+)
 
 SELECT col_name FROM collection c
 JOIN track_collection tc ON c.id = tc.collection_id
